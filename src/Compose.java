6fa69b3157f9c10475b54f0a023f76b4e3e6c5b5
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-08-09 15:41:15 -0700

    very very rough copy but with functionality

--- src/Compose.java
+++ src/Compose.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Compose {
@@ -10,19 +7,64 @@ public class Compose {
 
 	public static void main(String[] args) {
 		String str = "";
+		int spaceCount = 0;
+		int lineCount = 0;
+		boolean end = false;
+		boolean start = true;
 		Compose c = new Compose();
 		database = c.createDatabase("testtrain.txt");
 		// TODO Auto-generated method stub
 		char currNote = database.charAt((int) (Math.random() * database.length()) - 1);
-		
+		while (currNote == '|' || currNote == ' ' || currNote == ':') {
+			currNote = database.charAt((int) (Math.random() * database.length()) - 1);
+		}
+
 		// System.out.println(database);
 		str = str + currNote;
-		while (currNote != ']') {
+		full: while (true) {
+			char origNote = currNote;
+			currNote = new FrequencyTable(origNote).getMaxNote();
+			while (currNote == ']') {
+				end = true;
+				currNote = new FrequencyTable(origNote).getMaxNote();
+			}
 
-			currNote = new FrequencyTable(currNote).getMaxNote();
-			str = str + currNote;
-			//System.out.print(currNote);
+			if (currNote == ' ' || currNote == '|' || currNote == ':') {
+
+			} else {
+				spaceCount++;
+				lineCount++;
+			}
+
+			String addition = "";
+			if (spaceCount == 3) {
+				addition = addition + " ";
+				spaceCount = 0;
+			}
+			if (lineCount == 6) {
+				if (end) {
+					addition = addition + "|]";
+				} else {
+					addition = addition + "| ";
+				}
+				lineCount = 0;
+			}
+			if (currNote == ' ' || currNote == '|' || currNote == ':') {
+			} else {
+				str = str + currNote + addition;
+				if (end) {
+					str = str.substring(0, str.length()-1) + currNote + addition;
+					System.out.println("this");
+					break full;
+				}else if(start){
+					str = str + addition;
+					start = false;
+				}
+			}
+			System.out.println(str);
+			// System.out.print(currNote);
 		}
+		System.out.println("X:1\nT:I made this\nM:6/8\nL:1/8\nR:jig\nK:G");
 		System.out.println(str);
 
 	}
@@ -36,15 +78,15 @@ public class Compose {
 		String returnString = "";
 		try {
 			in = new Scanner(new FileReader(fileName));
-			
-			while (in.hasNextLine()) {returnString = returnString + "\n" + in.nextLine();}
+
+			while (in.hasNextLine()) {
+				returnString = returnString + in.nextLine();
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 		return returnString;
 	}
 
